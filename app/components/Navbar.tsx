bc4b694a65d965ffba03ede08f537450a961a596
Adem Ahmed Bekar <109433979+ademahmed23@users.noreply.github.com>
2026-01-01 00:59:47 +0300

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -40,7 +40,7 @@ export default function Navbar() {
       <div className="w-full bg-background mx-auto px-4 py-4 flex justify-between items-center">
         {/* Logo or Brand Name */}
         <Link href="/" className="text-2xl font-bold text-white">
-          Adem's Portfolio
+          Adems Portfolio
         </Link>
 
         {/* Hamburger Menu (Mobile) */}
@@ -92,7 +92,7 @@ export default function Navbar() {
           onClick={(e) => e.stopPropagation()} // Prevent clicks inside the sidebar from closing it
         >
           <div className="absolute top-4 left text-gray-300 text-xl focus:outline-none">
-            Adem's Portfolio
+            Adems Portfolio
           </div>
           {/* Close Button */}
           <button
